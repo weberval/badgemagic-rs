07a3b5731be87cd29daebb39687fd839224a16b6
Valentin Weber <valentin@wv2.ch>
2025-07-20 12:57:01 +0200

    fix: fix clippy linting part II

--- src/main.rs
+++ src/main.rs
@@ -245,7 +245,7 @@ fn write_payload(
             .enable_all()
             .build()?
             .block_on(async {
-                BleDevice::single(device_name.map(|x| x.as_str()))
+                BleDevice::single(device_name.map(String::as_str))
                     .await?
                     .write(payload)
                     .await
